a09b67767a9857eeb640518aae9e19fbbf4991a0
Kushal Amatya <kushalamatya413@gmail.com>
2023-08-02 20:41:29 +0545

    Conditional Statement

---
+++ conditional.py
@@ -0,0 +1,18 @@
+#if else
+a = 10
+
+if a>20:
+    print('Yes')
+else:
+    print('No')
+
+
+#switch
+choice = 2
+match choice:
+    case 1:
+        print('No1')
+    case 2:
+        print("No2")
+    case 3:
+        print('No3')
