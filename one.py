a0958fd25b52d1d8f35029e3e8acd16c7b89d8c1
Kushal Amatya <kushalamatya413@gmail.com>
2023-08-02 17:44:29 +0545

    First program

---
+++ one.py
@@ -0,0 +1,17 @@
+#basic print
+print("My name is Kushal Amatya")
+
+#formating with .format
+text = 'My name is {}'.format('Kushal Amatya')
+print(text)
+
+txt = '{a} = {b}'.format(a='Kushal', b='DadaDon')
+print(txt)
+
+#formatting using f-string
+t1 = 'Kushal'
+print(f'My name is {t1}')
+
+#methods
+print(len(t1))
+print(text.upper())
