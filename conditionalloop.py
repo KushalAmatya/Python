71d3672c05aab6e144e34fb37b8d66df22ad20df
Kushal Amatya <kushalamatya413@gmail.com>
2023-08-03 20:45:28 +0545

    Loops

---
+++ conditionalloop.py
@@ -0,0 +1,18 @@
+a = 5
+#while loop
+while a>=1:
+    print(f'value is {a}')
+    a=a-1
+
+
+#for loop
+for x in 'Kushal':
+    print(x)
+
+
+fruit = ["Apple", "Banana","Mango"]
+for x in fruit:
+    print(x)
+
+for x in range(5):
+    print(x)
